8d469917b9904569624f09296341fa34b6d1df0b
shauryacse17 <146994250+shauryacse17@users.noreply.github.com>
2025-03-30 16:23:12 +0530

    db.js

--- config/db.js
+++ config/db.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-// Function to establish a connection to the MongoDB database
+ 
 const connectDB = async () => {
     try {
-        // Attempt to connect to the MongoDB database
+         
         const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, // Use new URL string parser
-            useUnifiedTopology: true, // Use the new topology engine
+            useNewUrlParser: true,  
+            useUnifiedTopology: true,  
         });
 
-        // Log a success message with the host name
+         
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        // Log the error message if connection fails
+         
         console.error(`Error connecting to MongoDB: ${error.message}`);
-        process.exit(1); // Exit the process with failure code
+        process.exit(1);  
     }
 };
 
