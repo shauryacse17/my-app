1de3b8c92419de348ef82a9aefa7a16bee83d5a8
shauryacse17 <146994250+shauryacse17@users.noreply.github.com>
2025-04-06 19:02:08 +0530

    auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -61,7 +61,6 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-
 const router = express.Router();
 router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
@@ -88,30 +87,20 @@ router.post('/register', async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 });
-
-
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
-
   try {
-  
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
-
-   
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
-
-    
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
-
-   
     res.status(200).json({
       message: 'Login successful',
       username: user.name,
